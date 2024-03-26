4ed0a71da216d74943bb684bb5c1963f08d33e3a
Fangchen Ye <Fangchen.Ye@pax.us>
2024-03-26 17:34:21 -0400

    table design

--- sql/problem_submit_table.sql
+++ sql/problem_submit_table.sql
@@ -10,6 +10,6 @@ create table if not exists problem_submit
     createTime    datetime default CURRENT_TIMESTAMP not null comment '提交时间',
     updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete      tinyint  default 0                 not null comment '是否删除',
-    index idx_postId (problemId),
+    index idx_problemId (problemId),
     index idx_userId (userId)
 ) comment '题目提交表';
