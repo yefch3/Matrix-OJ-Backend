b0753449bf63c89a269f1082a09367aa7f594705
Fangchen Ye <Fangchen.Ye@pax.us>
2024-03-26 18:13:58 -0400

    problem api dev

---
+++ src/main/java/com/fangchen/oj/model/dto/problem/ProblemAddRequest.java
@@ -0,0 +1,33 @@
+package com.fangchen.oj.model.dto.problem;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 创建请求
+ *
+ * @author <a href="https://www.linkedin.com/in/fangchen-ye/i">fangchen</a>
+ * @from <a href="https://www.linkedin.com/in/fangchen-ye/">fangchen</a>
+ */
+@Data
+public class ProblemAddRequest implements Serializable {
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/fangchen/oj/model/dto/problem/ProblemEditRequest.java
@@ -0,0 +1,38 @@
+package com.fangchen.oj.model.dto.problem;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 编辑请求
+ *
+ * @author <a href="https://www.linkedin.com/in/fangchen-ye/i">fangchen</a>
+ * @from <a href="https://www.linkedin.com/in/fangchen-ye/">fangchen</a>
+ */
+@Data
+public class ProblemEditRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    private static final long serialVersionUID = 1L;
+}
